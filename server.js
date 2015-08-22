9639c334990255920bb2b160bf49770e9b637311
Taylor Denouden <taylordenouden@gmail.com>
2015-08-21 21:00:27 -0700

    Hookup basic angular app to request tweet stream from backend

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var app = express();
-var io = require('socket.io');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var twitter = require('twitter');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + "/html/index.html");
-})
+  res.sendFile(__dirname + '/html/index.html');
+});
 
-app.listen(3000);
-console.log("Server listening at 127.0.0.1:3000");
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('get tweets', function(track){
+    console.log(track);
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
