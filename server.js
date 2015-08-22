676a9a6c534189a2f0b4d286c8b83ceb41b871a4
Taylor Denouden <taylordenouden@gmail.com>
2015-08-21 21:46:23 -0700

    Initialize twitter stream service when term is searched

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var twitter = require('twitter');
+var Twitter = require('twitter');
+var twitter_config = require('./secret_twitter')
+
+app.use('/static', express.static('static'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/index.html');
 });
 
+var twitter_client = new Twitter(twitter_config);
+
 io.on('connection', function(socket){
   console.log('a user connected');
 
   socket.on('get tweets', function(track){
-    console.log(track);
-  });
+    console.log("'" + track + "' stream requested");
 
+    twitter_client.stream('statuses/filter', {track: track}, function(stream) {
+      stream.on('data', function(tweet) {
+        // if(tweet.coordinates){
+        socket.emit("new tweet", tweet);
+        // }
+      });
+
+      stream.on('error', function(error) {
+        throw error;
+      });
+    });
+  });
 });
 
 http.listen(3000, function(){
