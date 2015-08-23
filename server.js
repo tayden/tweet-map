702d6dfc9c11582789f3aa4bb9f5cd3299d927d7
Taylor Denouden <taylordenouden@gmail.com>
2015-08-22 21:21:26 -0700

    Cleanup server.js

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Twitter = require('twit');
-var twitter_config = require('./secret_twitter')
+
+// TODO: You must add your own twitter credentials here (see: https://apps.twitter.com/)
+var config = require('./secret_twitter');
+var twitter_client = new Twitter({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token: config.access_token,
+  access_token_secret: config.access_token_secret
+});
 
 app.use('/static', express.static('static'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/index.html');
 });
-
-var twitter_client = new Twitter(twitter_config);
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -24,9 +30,8 @@ io.on('connection', function(socket){
     if(stream){
       stream.stop();
     }
-
     stream = twitter_client.stream('statuses/filter', { track: track });
-    
+
     stream.on('tweet', function (tweet) {
       if(tweet.coordinates){
         socket.emit("new tweet", tweet);
